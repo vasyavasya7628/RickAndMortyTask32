640df6be8943ffd52e568cb558e13c0bf63ab032
vasyavasya7628 <mineok44@gmail.com>
2021-12-15 01:20:40 +0700

    Тренировал LiveData

--- app/src/main/java/com/example/rickandmortytask32/presentation/RickAndMortyFragment.kt
+++ app/src/main/java/com/example/rickandmortytask32/presentation/RickAndMortyFragment.kt
@@ -4,27 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.rickandmortytask32.R
-import com.example.rickandmortytask32.data.DataRickAndMortyNw
-import com.example.rickandmortytask32.data.RickAndMortyApi
 import com.example.rickandmortytask32.databinding.FragmentRickAndMortyBinding
 import com.example.rickandmortytask32.domain.DataRickAndMorty
-import com.example.rickandmortytask32.util.toDomain
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import timber.log.Timber
 
 class RickAndMortyFragment : Fragment() {
     private var _binding: FragmentRickAndMortyBinding? = null
     private val binding get() = _binding!!
-    private val api: RickAndMortyApi = RickAndMortyApi.create()
     private val characterAdapter: CharacterAdapter = CharacterAdapter()
-    private lateinit var rickAndMortyViewModel: RickAndMortyViewModel
+    private val rickAndMortyViewModel: RickAndMortyViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,43 +26,14 @@ class RickAndMortyFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        rickAndMortyViewModel = ViewModelProvider(this).get(RickAndMortyViewModel::class.java)
         initAdapter()
-        if (savedInstanceState == null) {
-            showToast(getString(R.string.loaded_from_net))
-            getDataFromNetwork()
-        } else {
-            showToast(getString(R.string.loaded_from_local_storage))
-            loadDataToAdapter(rickAndMortyViewModel.characters)
+        rickAndMortyViewModel.characters.observe(viewLifecycleOwner) {
+            loadDataToAdapter(it)
         }
+
+
     }
 
-
-    private fun getDataFromNetwork() {
-        api.getCharacterInfo(
-
-        ).enqueue(object : Callback<DataRickAndMortyNw> {
-            override fun onResponse(
-                call: Call<DataRickAndMortyNw>,
-                response: Response<DataRickAndMortyNw>
-            ) {
-                if (response.isSuccessful) {
-                    val character: DataRickAndMortyNw = response.body() as DataRickAndMortyNw
-                    val characterDomain: List<DataRickAndMorty> = character.list.map {
-                        it.toDomain()
-                    }
-                    rickAndMortyViewModel.setCharacters(characterDomain)
-                    loadDataToAdapter(characterDomain)
-                    Timber.d(characterDomain.toString())
-
-                } else Timber.d(response.message())
-            }
-
-            override fun onFailure(call: Call<DataRickAndMortyNw>, t: Throwable) {
-                Timber.d("CRASHED")
-            }
-        })
-    }
 
     fun loadDataToAdapter(characterDomain: List<DataRickAndMorty>) {
         val list = mutableListOf<CharacterRecyclerRickAndMorty>()
@@ -85,14 +46,6 @@ class RickAndMortyFragment : Fragment() {
     private fun initAdapter() {
         binding.recyclerviewRickandmorty.layoutManager = LinearLayoutManager(activity)
         binding.recyclerviewRickandmorty.adapter = characterAdapter
-    }
-
-    private fun showToast(message: String) {
-        Toast.makeText(
-            requireContext(),
-            message,
-            Toast.LENGTH_SHORT
-        ).show()
     }
 
     override fun onDestroy() {
