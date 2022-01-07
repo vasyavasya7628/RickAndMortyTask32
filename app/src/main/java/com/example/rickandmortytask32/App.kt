6600130d1e1c0c41854f4c6f085a77fab2fbb517
vasyavasya7628 <mineok44@gmail.com>
2022-01-07 23:50:20 +0700

    38 пытаюся сделать database

--- app/src/main/java/com/example/rickandmortytask32/App.kt
+++ app/src/main/java/com/example/rickandmortytask32/App.kt
@@ -2,14 +2,19 @@ package com.example.rickandmortytask32
 
 
 import android.app.Application
+import androidx.room.RoomDatabase
+import com.example.rickandmortytask32.data.AppDatabase
+import com.example.rickandmortytask32.data.CharacterDataBase
 import com.example.rickandmortytask32.data.RickAndMortyApi
 import timber.log.Timber
 
 class App() : Application() {
     companion object{
         private lateinit var api: RickAndMortyApi
+        private lateinit var characterDb: CharacterDataBase
     }
     override fun onCreate() {
+        characterDb = CharacterDataBase.create(applicationContext)
         api = RickAndMortyApi.create()
         Timber.plant(Timber.DebugTree())
         super.onCreate()
